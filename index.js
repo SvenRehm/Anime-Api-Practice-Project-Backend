3fba33fdbfbc96ac8fa5a1ab1dd9c74817dc7674
SvenRehm <rehm21@web.de>
2019-04-25 18:45:44 +0200

    new animelist  table

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const ExtractJwt = passportJWT.ExtractJwt
 const parser = require("body-parser")
 const knex = require("knex")
 const knexDb = knex({
-  client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "test",
-    database: "animebrain"
-  }
+   client: "pg",
+   connection: {
+      host: "127.0.0.1",
+      user: "postgres",
+      password: "Becher167!",
+      database: "animebrain"
+   }
 })
 const bookshelf = require("bookshelf")
 const securePassword = require("bookshelf-secure-password")
@@ -25,135 +25,201 @@ const db = bookshelf(knexDb)
 db.plugin(securePassword)
 const jwt = require("jsonwebtoken")
 const User = db.Model.extend({
-  tableName: "login_user",
-  hasSecurePassword: true
+   tableName: "login_user",
+   hasSecurePassword: true
 })
 
 const opts = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.SECRET_OR_KEY
+   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+   secretOrKey: process.env.SECRET_OR_KEY
 }
 
 const strategy = new JwtStrategy(opts, (payload, next) => {
-  User.forge({ id: payload.id })
-    .fetch()
-    .then(res => {
-      next(null, res)
-    })
+   User.forge({ id: payload.id })
+      .fetch()
+      .then(res => {
+         next(null, res)
+      })
 })
 app.use(cors())
 passport.use(strategy)
 app.use(passport.initialize())
 app.use(
-  parser.urlencoded({
-    extended: false
-  })
+   parser.urlencoded({
+      extended: false
+   })
 )
 app.use(parser.json())
 
 app.get("/", (req, res) => {
-  // res.send(knexDb.database)
-  knexDb
-    .select("id", "username", "animelist", "email", "joined")
-    .from("login_user")
-    .then(user => {
-      res.json(user)
-    })
+   // res.send(knexDb.database)
+   knexDb
+      .select("id", "username", "animelist", "email", "joined", "animelist2")
+      .from("login_user")
+      .then(user => {
+         res.json(user)
+      })
 })
 
 app.post("/register", (req, res) => {
-  if (!req.body.email || !req.body.password || !req.body.username) {
-    return res.status(401).send({ message: "Some Fields are empty" })
-  }
+   if (!req.body.email || !req.body.password || !req.body.username) {
+      return res.status(401).send({ message: "Some Fields are empty" })
+   }
 
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    joined: new Date()
-  })
+   const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      joined: new Date()
+   })
 
-  user
-    .save()
-    .then(() => {
-      res.send({ message: "Successfully Registered" })
-    })
-    .catch(err => {
-      return res
-        .status(401)
-        .send({ message: "Username or Email exists already" })
-    })
+   user
+      .save()
+      .then(() => {
+         res.send({ message: "Successfully Registered" })
+      })
+      .catch(err => {
+         return res
+            .status(401)
+            .send({ message: "Username or Email exists already" })
+      })
 })
 app.post("/getToken", (req, res) => {
-  if (!req.body.email || !req.body.password) {
-    return res.status(401).send({ message: "Some Fields are empty" })
-  }
-  User.forge({ email: req.body.email })
-    .fetch()
-    .then(result => {
-      if (!result) {
-        return res.status(400).send("user not found")
-      }
-      result
-        .authenticate(req.body.password)
-        .then(user => {
-          const payload = { id: user.id }
-          const token = jwt.sign(payload, process.env.SECRET_OR_KEY)
-          res.send(token)
-        })
-        .catch(err => {
-          return res
-            .status(401)
-            .send({ message: "Username or Password Incorrect" })
-        })
-    })
+   if (!req.body.email || !req.body.password) {
+      return res.status(401).send({ message: "Some Fields are empty" })
+   }
+   User.forge({ email: req.body.email })
+      .fetch()
+      .then(result => {
+         if (!result) {
+            return res.status(400).send("user not found")
+         }
+         result
+            .authenticate(req.body.password)
+            .then(user => {
+               const payload = { id: user.id }
+               const token = jwt.sign(payload, process.env.SECRET_OR_KEY)
+               res.send(token)
+            })
+            .catch(err => {
+               return res
+                  .status(401)
+                  .send({ message: "Username or Password Incorrect" })
+            })
+      })
 })
 
 app.get(
-  "/protected",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.send("i'm protected")
-  }
+   "/protected",
+   passport.authenticate("jwt", { session: false }),
+   (req, res) => {
+      res.send("i'm protected")
+   }
 )
 app.get(
-  "/getUser",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.send(req.user)
-  }
+   "/getUser",
+   passport.authenticate("jwt", { session: false }),
+   (req, res) => {
+      res.send(req.user)
+   }
 )
 
-app.put("/addplaylist", (req, res) => {
-  const { id, animeid } = req.body
-  knexDb("login_user")
-    .where("id", "=", id)
-    .update({
-      animelist: knexDb.raw("array_append(animelist, ?)", [animeid])
-    })
+app.get(
+   "/animelist",
+   passport.authenticate("jwt", { session: false }),
+   (req, res) => {
+      knexDb
+         .select("*")
+         .from("animelist")
+         .then(animelist => {
+            res.json(animelist)
+         })
+   }
+)
+// app.put("/addplaylist", (req, res) => {
+//   const { id, animeid } = req.body
+//   knexDb("login_user")
+//     .where("id", "=", id)
+//     .update({
+//       animelist: knexDb.raw("array_append(animelist, ?)", [animeid])
+// animelist2: knexDb.raw(`jsonb_set(??, '{animelist}', ?)`, ['animelist2', req.body.animelist])
+//     })
 
-    .returning("animelist")
-    .then(animelist => {
-      res.json(animelist[0])
-    })
-    .catch(err => res.status(400).json("unable to get animelist"))
+//     .returning("animelist")
+//     .then(animelist => {
+//       res.json(animelist[0])
+//     })
+//     .catch(err => res.status(400).json("unable to get animelist"))
+
+// })
+
+app.put("/addplaylist", (req, res) => {
+   const { id, anime_id, status, episodes_watched, notes } = req.body
+   knexDb("animelist")
+      .insert([
+         {
+            id: id,
+            anime_id: anime_id,
+            status: status,
+            episodes_watched: episodes_watched,
+            notes: notes
+         }
+      ])
+      .returning("*")
+      .then(animelist2 => {
+         res.json(animelist2[0])
+      })
+      .catch(err => res.status(400).json("unable to get animelist"))
+   // .catch(err=>console.log(err))
+})
+app.put("/update", (req, res) => {
+   const { id, anime_id, status, episodes_watched, notes } = req.body
+   knexDb("animelist")
+      .where("id", "=", id)
+      .where("anime_id", "=", anime_id)
+      .update({
+         status: status,
+         episodes_watched: episodes_watched
+      })
+
+      .returning("*")
+      .then(animelist2 => {
+         res.json(animelist2[0])
+      })
+      .catch(err => res.status(400).json("unable to get animelist"))
+   // .catch(err=>console.log(err))
 })
 
 app.delete("/removefromplaylist", (req, res) => {
-  const { id, animeid } = req.body
-
-  knexDb("login_user")
-    .where("id", "=", id)
-    .update({
-      animelist: knexDb.raw(`array_remove(animelist, ${animeid})`)
-    })
-
-    .returning("animelist")
-    .then(animelist => {
-      res.json(animelist[0])
-    })
-    .catch(err => res.status(400).json("unable to get animelist"))
+   const { id, anime_id } = req.body
+   knexDb("animelist")
+      .where("id", "=", id)
+      .where("anime_id", "=", anime_id)
+      .del()
+      .returning("*")
+      .then(animelist => {
+         res.json(animelist[0])
+      })
+      .catch(err => res.status(400).json("unable to get animelist"))
 })
+// app.delete("/removefromplaylist", (req, res) => {
+//   const { id, animeid } = req.body
+
+//   knexDb("login_user")
+//     .where("id", "=", id)
+
+//     .update({
+//       animelist: knexDb.raw(`array_remove(animelist, ${animeid})`)
+//     })
+
+//     .returning("animelist")
+//     .then(animelist => {
+//       res.json(animelist[0])
+//     })
+
+//     .catch(err =>  res.status(400).json("unable to get animelist"))
+
+// })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
